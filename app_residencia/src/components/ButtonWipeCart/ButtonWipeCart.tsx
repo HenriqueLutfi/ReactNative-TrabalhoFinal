916221252dffdd9a36a2148860dcdcd375b43686
Henrique Lutfi <henrique_lutfi@hotmail.com>
2022-07-05 21:55:48 -0300

    bot finalizar compra + alerts

--- app_residencia/src/components/ButtonWipeCart/ButtonWipeCart.tsx
+++ app_residencia/src/components/ButtonWipeCart/ButtonWipeCart.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import {Button} from 'react-native-elements';
 import {CartContext} from '../../context/CartContext';
 import {CarrinhoContext} from '../../context/CarrinhoContext';
-
+import { styles } from './styles';
 export const ButtonWipeCart = () => {
   // const {setCart} = useContext(CartContext);
   const {LimparCarrinho} = useContext(CarrinhoContext);
@@ -10,14 +10,13 @@ export const ButtonWipeCart = () => {
     <>
       <Button
         title="limpar carrinho"
-        titleStyle={{
-          color: '#fff700',
-        }}
+        titleStyle={styles.textButton}
         buttonStyle={{
           backgroundColor: '#000000',
           borderWidth: 2,
           borderColor: '#fff700',
           borderRadius: 3,
+          padding: 5,
         }}
         // onPress={() => {
         //   setCart([]);
--- app_residencia/src/components/FinishPurchase/FinishPurchase.tsx
+++ app_residencia/src/components/FinishPurchase/FinishPurchase.tsx
@@ -1,18 +1,20 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {View} from 'react-native';
+import {View, Alert} from 'react-native';
 import {Button, Text} from 'react-native-elements';
 import {CartContext} from '../../context/CartContext';
 import {ButtonWipeCart} from '../ButtonWipeCart/ButtonWipeCart';
 import {styles} from './style';
 import {CarrinhoContext} from '../../context/CarrinhoContext';
+import {TouchableHighlight} from 'react-native-gesture-handler';
 
 export const FinishPurchase = () => {
   // const {cart} = useContext(CartContext);
-  const {contarQtdProdutos, listarProdutos, ValorTotalCarrinho} = useContext(CarrinhoContext);
+  const {contarQtdProdutos, listarProdutos, ValorTotalCarrinho, LimparCarrinho} =
+    useContext(CarrinhoContext);
 
   const [somaTotal, setSomaTotal] = useState(0);
   const [carrinho, setCarrinho] = useState();
-  const [qtd, setQtd] = useState()
+  const [qtd, setQtd] = useState();
   // console.log(cart);
   useEffect(() => {
     SomaProdutos();
@@ -24,8 +26,8 @@ export const FinishPurchase = () => {
 
   const getDadosCarrinho = () => {
     setCarrinho(listarProdutos());
-    setQtd(contarQtdProdutos())
-    setSomaTotal(ValorTotalCarrinho())
+    setQtd(contarQtdProdutos());
+    setSomaTotal(ValorTotalCarrinho());
   };
 
   function SomaProdutos() {
@@ -40,16 +42,47 @@ export const FinishPurchase = () => {
     //   soma += parseFloat(cart[i].precoProduto);
     // }
   }
-  
+  function showAlert() {
+    Alert.alert('Compra', 'Compra Finalizada com sucesso', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log(LimparCarrinho())},
+    ]);
+  }
 
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.textColor}>
-          quantidade de produtos {qtd}
-        </Text>
+        {/* <View style={styles.viewText}> */}
+        <Text style={styles.textColor}>quantidade de produtos {qtd}</Text>
         <Text style={styles.textColor}>Valor Total R${somaTotal},00</Text>
-        <ButtonWipeCart />
+        {/* </View> */}
+        <View style={styles.viewButtonsRow}>
+          <View style={styles.viewButtons}>
+            <Button
+              title="Finalizar Compra"
+              titleStyle={styles.textButton}
+              buttonStyle={{
+                backgroundColor: '#000000',
+                borderWidth: 2,
+                borderColor: '#fff700',
+                borderRadius: 3,
+                padding: 5,
+              }}
+              // onPress={() => {
+              //   setCart([]);
+              onPress={() => {
+                showAlert();
+              }}
+            />
+          </View>
+          <View style={styles.viewButtons}>
+            <ButtonWipeCart />
+          </View>
+        </View>
       </View>
     </View>
   );
